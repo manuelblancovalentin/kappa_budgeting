88f34d0b74d44139b6322476f12466c2e03dcaaf
manuelblancovalentin <manuel.blanco.valentin@gmail.com>
2026-05-15 18:29:04 -0500

    Add train emoji to ENABOL brand

--- site/src/components/BrandName/index.js
+++ site/src/components/BrandName/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function BrandName({children = 'ENABOL'}) {
+export default function BrandName({children = '🚂ENABOL'}) {
   return <span className="brand-name">{children}</span>;
 }
